eddac3ebe4b9c23f7acd89bda687e27e760cbd04
Deyi Kong <deyikong@yahoo.com>
2018-05-26 17:23:54 -0500

    Added class notes

---
+++ HelloWorld/ConditionStatement/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1000;
+            string itemName = "Cat";
+
+            // if count is less than 1000
+            if (!(itemName == "Cat" || count < 1000))
+            {
+                Console.WriteLine("You need to buy more");
+            }
+            // if count is equal to or greater than 1000
+            else if (count == 1000)
+            {
+                Console.WriteLine("You have 1000 items.");
+            }
+            else
+            {
+                Console.WriteLine("You are good on the quantity");
+            }
+
+
+            //Switch: Can only be used for "equal" evaluation.
+            string today = "Sun.";
+            switch (today)
+            {
+                case "Sun.":
+                case "Sat.":
+                    Console.WriteLine("Yay Weekend!");
+                    break;
+                default:
+                    Console.WriteLine("Sad day, keep working hard");
+                    break;
+            }
+
+            //User should see "The item count is 10"
+            Console.WriteLine("The item count is " + count);
+            Console.WriteLine("The total count is " + count);
+
+            // ? : ternary operatoer
+            // condition ? first_expression : second_expression; 
+            /*
+             * if ( count > 1000)
+             * {
+             *  Console.WriteLine("Greater than 1000");
+             * }else
+             * {
+             *  Console.WriteLine("Less than or equal to 1000");
+             * }
+             */
+            Console.WriteLine(count > 1000 ? "Greater than 1000" : "Less than or equal to 1000");
+        }
+    }
+}
---
+++ HelloWorld/IfStatementPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatementPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            //#region
+            string age = "";
+            string firstName = "";
+            string lastName = "";
+            string gender = "";
+            string greetGender = "";
+            string greeting = "";
+            var time = DateTime.Now.Hour;
+            //#endregion
+
+            Console.WriteLine("Please enter your first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your last name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Please enter your age:");
+            age = Console.ReadLine();
+            Console.WriteLine("Please enter your gender:");
+            gender = Console.ReadLine();
+            greetGender = gender == "male" || gender == "Male" ? "Mr. " : "Miss ";
+
+            greeting = time > 8 && time < 18 ? "Good day, " : "Good evening, ";
+            Console.WriteLine(greeting + greetGender + lastName + ", Age " + age);
+        }
+    }
+}
